cf6eee715696713ef0a472e01267bee5a9b99b31
Samithra <samithrasamarasinghe@gmail.com>
2023-05-21 21:11:40 +0530

    Change the title in form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -64,7 +64,7 @@ const Form = ({ currentId, setCurrentId }) => {
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">
-          {currentId ? "Editing " : "Creating "}a Memory
+          {currentId ? "Editing " : "Creating "}your post
         </Typography>
         <TextField
           name="title"
